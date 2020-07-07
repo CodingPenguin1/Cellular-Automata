295a7c88cffab27c85bb295d5f7293ee6a60b65b
Ryan Slater <ryan.j.slater.2@gmail.com>
2020-07-07 13:57:21 -0400

    GUI render and multithreading world updating

--- World.py
+++ World.py
@@ -3,6 +3,8 @@
 
 """
 
+import concurrent.futures
+from multiprocessing import cpu_count
 from random import random
 
 import numpy as np
@@ -11,9 +13,9 @@ from tabulate import tabulate
 
 class World:
     def __init__(self, size=(720, 480)):
-        self.grid = np.zeros(size, dtype=np.uint8)
         self.width = size[0]
         self.height = size[1]
+        self.grid = np.zeros((self.height, self.width), dtype=np.uint8)
 
     def toggleCell(self, coords):
         self.grid[coords[0]][coords[1]] = 0 if self.grid[coords[0]][coords[1]] else 1
@@ -25,49 +27,54 @@ class World:
                     self.toggleCell((row, col))
 
     def updateConways(self):
-        def getNeighborCount(array, coords):
-            count = 0 if array[coords[0]][coords[1]] == 0 else -1
-            for row in range(coords[0] - 1, coords[0] + 2):
-                for col in range(coords[1] - 1, coords[1] + 2):
-                    if row >= 0 and row < self.height and col >= 0 and col < self.width:
-                        if self.grid[row][col] == 1:
-                            count += 1
-            return count
-
-        nextBoardState = np.zeros((self.width, self.height), dtype=np.uint8)
-
-        # Determine new state for each cell and set on new board
-        for row in range(self.height):
-            for col in range(self.width):
-                neighbors = getNeighborCount(self.grid, [row, col])
-                if self.grid[row][col] == 1:
-                    # Any live cell with fewer than two live neighbours dies
-                    if neighbors < 2:
-                        nextBoardState[row][col] = 0
-                    # Any live cell with two or three live neighbours lives on to the next generation
-                    if neighbors == 2 or neighbors == 3:
-                        nextBoardState[row][col] = 1
-                    # Any live cell with more than three live neighbours dies
-                    else:
-                        nextBoardState[row][col] = 0
-                else:
-                    # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-                    if neighbors == 3:
-                        nextBoardState[row][col] = 1
+        # Generate next board state
+        nextBoardState = np.zeros((self.height, self.width), dtype=np.uint8)
+        with concurrent.futures.ProcessPoolExecutor(cpu_count()) as executor:
+            rows = [executor.submit(_generateRow, self.grid, i).result() for i in range(len(self.grid))]
+        for row in rows:
+            nextBoardState[row['index']] = row['values']
         self.grid = np.copy(nextBoardState)
 
     def __str__(self):
         return tabulate(self.grid, tablefmt='plain')
 
 
+def _getNeighborCount(array, coords):
+    count = 0 if array[coords[0]][coords[1]] == 0 else -1
+    for row in range(coords[0] - 1, coords[0] + 2):
+        for col in range(coords[1] - 1, coords[1] + 2):
+            if row >= 0 and row < len(array) and col >= 0 and col < len(array[0]):
+                if array[row][col] == 1:
+                    count += 1
+    return count
+
+
+def _generateRow(array, rowIndex):
+    row = np.copy(array[rowIndex])
+    for col in range(len(array[rowIndex])):
+        neighbors = _getNeighborCount(array, [rowIndex, col])
+        if array[rowIndex][col] == 1:
+            # Any live cell with fewer than two live neighbours dies
+            if neighbors < 2:
+                row[col] = 0
+            # Any live cell with two or three live neighbours lives on to the next generation
+            if neighbors == 2 or neighbors == 3:
+                row[col] = 1
+            # Any live cell with more than three live neighbours dies
+            else:
+                row[col] = 0
+        else:
+            # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            if neighbors == 3:
+                row[col] = 1
+            else:
+                row[col] = 0
+    return {'index': rowIndex, 'values': row}
+
+
 if __name__ == '__main__':
-    world = World((10, 10))
-    world.grid[7][2] = 1
-    world.grid[7][3] = 1
-    world.grid[8][3] = 1
+    world = World((10, 2))
     print(world)
     while input() != 'q':
         world.updateConways()
         print(world)
-
-
--- main.py
+++ main.py
@@ -2,29 +2,53 @@
 """
 """
 
+from time import time
+
 import pygame
+
+from World import World
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+WORLD_SIZE = (500, 500)
 
 
 if __name__ == '__main__':
+    # Initialization
     pygame.init()
-    window = pygame.display.set_mode((720, 480))
+    window = pygame.display.set_mode(WORLD_SIZE)
     clock = pygame.time.Clock()
-    FPS = 60
+    world = World((WORLD_SIZE))
+    targetFramerate = 1
+    world.randomize()
 
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-
-    rect = pygame.Rect((0, 0), (32, 32))
-    image = pygame.Surface((32, 32))
-    image .fill(WHITE)
-
+    # Game loop
     while True:
-        clock.tick(FPS)
-
+        # Input
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     quit()
 
-        pygame.draw.rect(window, WHITE, (0, 0, 100, 100))
+        # Render frame
+        print('Rendering frame')
+        t0 = time()
+        window.fill(WHITE)
+        for x in range(world.width):
+            for y in range(world.height):
+                if world.grid[y][x] == 1:
+                    pygame.draw.rect(window, BLACK, (x, y, 1, 1))
+        print(f'{round(time() - t0, 2)}s')
+
+        # Update world
+        print('Updating world')
+        t0 = time()
+        world.updateConways()
+        print(f'{round(time() - t0, 2)}s')
+
+        # Update clock and display
+        print('Displaying frame')
+        t0 = time()
+        clock.tick(targetFramerate)
         pygame.display.update()
+        print(f'{round(time() - t0, 2)}s')
