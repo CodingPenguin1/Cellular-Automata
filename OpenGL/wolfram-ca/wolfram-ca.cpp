d6f9385bd35879a81746515eef0ffade53b73888
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-05-26 15:22:54 -0400

    its sorta drawing stuff

--- OpenGL/wolfram-ca/wolfram-ca.cpp
+++ OpenGL/wolfram-ca/wolfram-ca.cpp
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 
-#define WIDTH 1000
+#define WIDTH 1001
 #define HEIGHT 1000
 
 
@@ -20,6 +20,10 @@ uint8_t current_buffer = 0;
 double start_time;
 const int framerate_average_count = 5;
 double *frame_times = new double[framerate_average_count];
+uint8_t rule = 250;
+const uint32_t OFF_COLOR = 0x000000ff;
+const uint32_t ON_COLOR = 0xffffffff;
+int row = 0;
 
 
 double time() {
@@ -34,14 +38,60 @@ void draw_string(float x, float y, std::string s) {
 }
 
 
-void flip_grid() {
-    // Switches the grid buffers
-    current_buffer = current_buffer ? 0 : 1;
-}
-
-
 void update_grid() {
-    flip_grid();
+    // Based on work by The Coding Train on YouTube
+    // https://www.youtube.com/watch?v=W1zKu3fDQR8
+    uint8_t next_buffer = current_buffer ? 0 : 1;
+
+    // If current row is bottom row, shift all rows up 1
+    if (row == HEIGHT - 1)
+        for (int i = WIDTH; i < WIDTH * HEIGHT; ++i)
+            grid[current_buffer][i - WIDTH] = grid[current_buffer][i];
+
+    // Copy current buffer to next buffer
+    for (int i = 0; i < WIDTH * HEIGHT; ++i)
+        grid[next_buffer][i] = grid[current_buffer][i];
+
+    for (int col = 0; col < HEIGHT; ++col) {
+        // Get state of current cell
+        int i = row * WIDTH + col;
+        uint8_t current_cell_state = 0b00000000;
+        if (grid[current_buffer][i] > OFF_COLOR)
+            current_cell_state = 0b00000010;
+
+        // Get state of cell left of current cell
+        int left_cell_index = i - 1;
+        if (col == 0)
+            left_cell_index = row * WIDTH + (HEIGHT - 1);
+        uint8_t left_cell_state = 0b00000000;
+        if (grid[current_buffer][left_cell_index] > OFF_COLOR)
+            left_cell_state = 0b00000100;
+
+        // Get state of cell right of current cell
+        int right_cell_index = i + 1;
+        if (col == HEIGHT - 1)
+            right_cell_index = row * WIDTH;
+        uint32_t right_cell_state = 0b00000000;
+        if (grid[current_buffer][right_cell_index] > OFF_COLOR)
+            right_cell_state = 0b00000001;
+
+        // Combine into a single value of neighborhood configuration
+        uint8_t configuration = left_cell_state | current_cell_state | right_cell_state;
+
+        // Get next generation state for current cell column (value to put in next row, same column as current cell)
+        uint8_t next_state = (rule & (0x01 << configuration)) >> configuration;
+        if (next_state)
+            grid[next_buffer][i] = ON_COLOR;
+        else
+            grid[next_buffer][i] = OFF_COLOR;
+
+
+        grid[next_buffer][WIDTH * row + row] = 0xff000000;
+    }
+
+    if (row < HEIGHT)
+        ++row;
+    current_buffer = next_buffer;
 }
 
 
@@ -64,7 +114,7 @@ void display() {
     // Render settings text
     glPushMatrix();
     glLoadIdentity();
-    glColor3f(1.0, 1.0, 1.0);
+    glColor3f(0.0, 0.0, 1.0);
     draw_string(-0.99, 0.97, std::to_string((int)framerate) + " FPS");
     glPopMatrix();
 
@@ -77,7 +127,7 @@ void display() {
 void init(int argc, char **argv) {
     // Set up window
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(WIDTH, HEIGHT);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("[floating] Cellular Automata");
@@ -94,11 +144,14 @@ void init(int argc, char **argv) {
 
     // Create empty grid
     for (int i = 0; i < WIDTH * HEIGHT; ++i) {
-        grid[0][i] = 0x00000000;
-        grid[1][i] = 0x00000000;
+        grid[0][i] = OFF_COLOR;
+        grid[1][i] = OFF_COLOR;
     }
 
-    // Initialize framerate average arrayN
+    // Put 1 pixel in the top row, center col
+    grid[current_buffer][WIDTH / 2] = ON_COLOR;
+
+    // Initialize framerate average array
     for (int i = 0; i < framerate_average_count; ++i)
         frame_times[i] = 0.0;
 }
